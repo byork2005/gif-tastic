3ff137e9bcc9f1ae254d5f05ef44659241294ab1
Brandon York <30988963+byork2005@users.noreply.github.com>
2017-10-23 17:00:24 -0500

    update

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,18 +1,44 @@
 $(document).ready(function()
 {
 
-var topics = ["Superman","Batman","Flash","Wonder Woman","Aquaman"];
+    var topics = ["Superman","Batman","The Flash","Wonder Woman","Aquaman"];
 
-function makeButtons() 
-{
-    $(".topicButtons").empty();
-    for (var i = 0; i < topics.length; i++)
+    function makeButtons() 
     {
-        var newButton = $("<button class='buttons'>");
-        newButton.text(topics[i]);
-        $(".topicButtons").append(newButton);
-    }    
-}
+        $(".topicButtons").empty();
+        for (var i = 0; i < topics.length; i++)
+        {
+            var newButton = $("<button class='buttons'>");
+            newButton.attr("data-value", topics[i])
+            newButton.text(topics[i]);
+            $(".topicButtons").append(newButton);
+        }    
+    }
+
+    makeButtons();
+
+    $(".buttons").on("click", function()
+    {
+        console.log("event")
+        var buttonTopic = $(this).attr("data-value")
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
+        buttonTopic + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+
+        $.ajax
+        ({
+            url: queryURL,
+            method: "GET"
+        })
+
+        .done(function(response) 
+        {
+            console.log(response)
+        })
+
+        // $( "#dataTable tbody" ).on( "click", "tr", function() {
+        //     console.log( $( this ).text() );
+        //   });    -example of jQuery on() handler
 
 
 
@@ -23,9 +49,8 @@ function makeButtons()
 
 
 
+    });
 
 
-
-makeButtons();
 
 });
